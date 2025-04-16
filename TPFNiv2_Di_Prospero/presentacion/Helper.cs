1b9edf730353ca53656dd46cdf02c3dcec221d83
jdipro <43705707+jdipro@users.noreply.github.com>
2025-04-16 11:30:45 -0300

    Correccion. Anexo helper y prueba ruta imagen_not_found. Ux

---
+++ TPFNiv2_Di_Prospero/presentacion/Helper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Negocio
+{
+    //Esta clase tendrá métodos comunes a ser llamados por otros métodos en proyectos previamnete referenciados.
+    public static class Helper                      
+    {
+        public static void CargarImagen(PictureBox pictureBox, string imagen)
+        {
+            try
+            {
+                pictureBox.Load(imagen);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al cargar la imagen: {ex.Message}");
+                
+                pictureBox.Load(@".\Media\image_not_found.png");
+            }
+        }
+    }
+}
